f503a0951cbe71df5ff1fa40c2278bb0ccafdee1
in100tiva <projetoin100tiva@gmail.com>
2025-05-14 15:52:36 -0300

    att hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,5 +2,13 @@ import "./Hero.css";
 
 
 export default function Hero() {
-
+    <section className="hero" id="home">
+        <div className="hero-content">
+            <h2>Bem vindo ao mundo de React</h2>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            </p>
+            <button className="cta-button">Iniciar estudos</button>
+        </div>
+    </section>
 }
