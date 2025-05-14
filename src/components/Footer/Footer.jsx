7a6de3565c7bcb4bf4062524440a67bc76c7e6b4
in100tiva <projetoin100tiva@gmail.com>
2025-05-14 16:12:38 -0300

    att footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,30 @@
 import "./Footer.css";
 
 export default function Footer() {
-    
+    <footer className="footer" id="contact">
+        <div className="footer-content">
+            <div className="footer-section">
+                <h3>Lorem ipsum dolor sit.</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi inventore sint ullam nisi!</p>
+            </div>
+
+            <div className="footer-section">
+                <h3>Lorem ipsum dolor.</h3>
+                <p>Email: email@hotmail.com</p>
+                <p>Phone: 85 9999-9999</p>
+            </div>
+
+            <div className="footer-section">
+                <h3>ipsum dolor.</h3>
+                <div>
+                    <a href="#">Instagram</a>
+                    <a href="#">Orkut</a>
+                    <a href="#">MSN</a>
+                </div>
+            </div>
+        </div>
+        <div className="footer-button">
+            <button>&copy; 2025 - Todos os direitos reservados as vozes da minha cabeça</button>
+        </div>
+    </footer>
 }
