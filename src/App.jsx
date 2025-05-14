3905758bd7c373714ca3d992906c1a6beccd5dfe
in100tiva <projetoin100tiva@gmail.com>
2025-05-14 15:23:55 -0300

    att

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import "./App.css"
-import Header from "./components/Header/Header"
-import Hero from "./components/Hero/Hero"
-import Features from "./components/Features/Features"
-import Footer from "./components/Footer/Footer"
+import "./App.css";
+import Header from "./components/Header/Header.jsx";
+import Hero from "./components/Hero/Hero.jsx";
+import Features from "./components/Features/Features.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 
-function App() {
-  return (
-    <div className="app">
-      <Header />
-      <Hero />
-      <Features />
-      <Footer />
-    </div>
-  )
+
+export default function App() {
+    return (
+        <div className="app">
+            <Header />
+            <Hero />
+            <Features />
+            <Footer />
+        </div>
+    )
 }
-
-export default App
